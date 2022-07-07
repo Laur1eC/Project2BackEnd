3106ef8e8074343aac9fcbec2ec78130239d52fe
Laurie C <50302752+Laur1eC@users.noreply.github.com>
2022-07-07 14:19:00 -0400

    added getFeaturedProducts and getProductsOnSale

--- src/main/java/com/revature/repositories/ProductRepository.java
+++ src/main/java/com/revature/repositories/ProductRepository.java
@@ -2,6 +2,14 @@ package com.revature.repositories;
 
 import com.revature.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Query("FROM Product WHERE featured = 'true'")
+    List<Product> getFeaturedProducts(boolean f);
+
+    @Query("FROM Product WHERE sale > 0")
+    List<Product> getProductsOnSale();
 }
